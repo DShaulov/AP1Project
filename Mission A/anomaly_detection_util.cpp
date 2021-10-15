4a0532749b169ce1dd76aaff3ae33f0c6dd62c5e
David Shaulov <shaulovl55@gmail.com>
2021-10-15 15:29:17 +0300

    Final

--- Mission A/anomaly_detection_util.cpp
+++ Mission A/anomaly_detection_util.cpp
@@ -3,27 +3,26 @@
 #include <iostream>
 #include <math.h>
 
-float var(float* x, int size) {
-	float sum = 0;
-	float sumOfSquares = 0;
+float mean(float* x, int size) {
+    float sum = 0;
 	for (int i = 0; i < size; i++) {
 		sum += x[i];
+	}
+    return (sum / size);
+}
+float var(float* x, int size) {
+	float sumOfSquares = 0;
+	for (int i = 0; i < size; i++) {
 		sumOfSquares += pow(x[i], 2);
 	}
-	float mean = pow(((1.0 / size) * sum), 2);
+	float meanSquared = pow(mean(x, size), 2);
 	float equationFirstPart = ((1.0 / size) * sumOfSquares);
-	return (equationFirstPart - mean);
+	return (equationFirstPart - meanSquared);
 }
 // 𝑐𝑜𝑣(𝑋,𝑌) = 𝐸((𝑋−𝐸(𝑋))(𝑌−𝐸(𝑌)), E is simple mean
 float cov(float* x, float* y, int size) {
-    float sumOfX = 0;
-    float sumOfY = 0;
-    for (int i = 0; i < size; i++) {
-		sumOfX += x[i];
-		sumOfY += y[i];
-	}
-    float meanOfX = sumOfX / size;
-    float meanOfY = sumOfY / size;
+    float meanOfX = mean(x, size);
+    float meanOfY = mean(y, size);
     float covSum = 0;
     for (int i = 0; i < size; i++) {
         covSum += (x[i] - meanOfX) * (y[i] - meanOfY);
@@ -39,10 +38,17 @@ Line linear_reg(Point** points, int size) {
     }
     float* xVals = new float[size];
     float* yVals = new float[size];
-
-
+    Point *pointArr = *points;
+    for (int i = 0; i < size; i++) {
+        xVals[i] = pointArr[i].x;
+        yVals[i] = pointArr[i].y;
+    }
+    float a = cov(xVals, yVals, size) / var(xVals, size);
+    float b = mean(yVals, size) - (a * mean(xVals, size));
     delete[] xVals;
     delete[] yVals;
+    Line line = Line(a, b);
+    return line;
 }
 
 // TESTS
@@ -69,7 +75,7 @@ int main() {
 
     // Expected: -12
     std::cout << cov(coTestArray1, coTestArray2, 5) << std::endl;
-    // Expected: 146.1
+    // Expected: 116.68
     std::cout << cov(coTestArray3, coTestArray4, 5) << std::endl;
 
     // Pearson tests:
@@ -77,5 +83,18 @@ int main() {
     std::cout << pearson(coTestArray3, coTestArray4, 5) << std::endl;
     // Expected: -0.9189
     std::cout << pearson(coTestArray2, coTestArray3, 5) << std::endl;
-
+    // Linear reg test:
+    Point *pointArray = (Point *) malloc(sizeof(Point) * 4);
+    Point *pointArray2 = (Point *) malloc(sizeof(Point) * 4);
+    pointArray[0] = Point(1, 2);
+    pointArray[1] = Point(2, 3);
+    pointArray[2] = Point(3, 4);
+    pointArray[3] = Point(4, 5);
+    pointArray[0] = Point(3, -50);
+    pointArray[1] = Point(2, 8);
+    pointArray[2] = Point(5, 20);
+    pointArray[3] = Point(8, 3);
+    Line line = linear_reg(&pointArray, 4);
+    Line line2 = linear_reg(&pointArray, 4);
+    std::cout << "Hello World" << std:: endl;
 }
